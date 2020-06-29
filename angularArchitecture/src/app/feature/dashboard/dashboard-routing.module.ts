9c500ce486c8aa79a88fcf165d0dfca13e1cf47a
Vijay Sherekar <vijaysherekar@gmail.com>
2020-06-29 15:11:49 +0530

    added the changes for sample component restructuring

--- angularArchitecture/src/app/feature/dashboard/dashboard-routing.module.ts
+++ angularArchitecture/src/app/feature/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SampleComponent } from './sample.component';
+import { SampleComponent } from './sample/sample.component';
 
 
 const routes: Routes = [{
--- angularArchitecture/src/app/feature/dashboard/dashboard.module.ts
+++ angularArchitecture/src/app/feature/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { SampleComponent } from './sample.component';
+import { SampleComponent } from './sample/sample.component';
 import {SharedModule} from './../../shared/shared.module'
 
 
