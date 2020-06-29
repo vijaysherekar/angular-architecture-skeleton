1761351a612a15c2821f774ad2dff46d6028657f
Vijay Sherekar <vijaysherekar@gmail.com>
2020-06-29 15:04:22 +0530

    Added service and data-service files for home component

--- angularArchitecture/src/app/app.module.ts
+++ angularArchitecture/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home.component';
+import { HomeComponent } from './home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from './shared/shared.module'
