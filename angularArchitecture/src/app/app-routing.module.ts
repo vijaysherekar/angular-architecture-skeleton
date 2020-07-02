e47c28e0847635433537b5c1b20439e3babbe559
Vijay Sherekar <vijaysherekar@gmail.com>
2020-07-02 18:31:42 +0530

    Added guards and interceptors by schemantics

--- angularArchitecture/src/app/app-routing.module.ts
+++ angularArchitecture/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { DashboardGuard } from './core/guards/dashboard/dashboard.guard';
 
 
 const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./feature/dashboard/dashboard.module').then(m => m.DashboardModule),
-    data: { preload: false }
+    data: { preload: false },
+    canLoad: [DashboardGuard]
   },
   {
     path: 'home',
--- angularArchitecture/src/app/core/http-interceptors/index.ts
+++ angularArchitecture/src/app/core/http-interceptors/index.ts
@@ -1,20 +1,12 @@
 /* "Barrel" of Http Interceptors */
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { AuthInterceptorService } from './auth-interceptor.service';
-
-
-import { LoggingInterceptorService } from './logging-interceptor.service';
-
-
-
+import { AuthInterceptor } from './auth.interceptor';
+import { LoggingInterceptor } from './logging.interceptor';
 
 
 /** Http interceptor providers in outside-in order */
 export const httpInterceptorProviders = [
-
-  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptorService, multi: true },
-
-
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true },
 ];
